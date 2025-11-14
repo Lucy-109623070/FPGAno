7ce46f3d7edf72dfb583e9e7602b4c4cf2e5ff82
Lucero Aguilar <158243865+Lucy-109623070@users.noreply.github.com>
2025-11-14 15:31:09 -0800

    Add files via upload

--- project_1/project_1.srcs/sources_1/new/FPGAno.v
+++ project_1/project_1.srcs/sources_1/new/FPGAno.v
@@ -1,27 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/14/2025 02:42:18 PM
-// Design Name: 
-// Module Name: FPGAno
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module FPGAno(
-    input btn,
-    output sw
-    );
+    input btn,   // input button
+    output led   // output LED
+);
+
+    assign led = btn;  // direct connection
+
 endmodule
